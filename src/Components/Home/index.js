76a72f50b4947cee63ca8779c6a1230736334758
Eric Chaves <erchaves@gmail.com>
2019-09-26 18:25:52 -0400

    first simple website

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -5,8 +5,31 @@ import './index.scss';
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        hello world
+      <div className={`page page-home`}>
+        <div className="h1-wrap">
+          <h1 className="h1">
+            Terr
+            <span className="bullet" />
+            a
+            <span className="bullet" />
+            ment
+          </h1>
+
+          <h2 className="h2">
+            Solving our multi-trillion dollar energy storage crisis
+          </h2>
+        </div>
+        <div className="footer">
+          <div className="footer-inner">
+            <span>
+              Coming soon.
+            </span>
+            <img src="/logo-white.png" alt="Terrament logo" />
+            <span>
+              Contact <a href="mailto:eric@syllablehq.com">eric@syllablehq.com</a> for details.
+            </span>
+          </div>
+        </div>
       </div>
     );
   }
