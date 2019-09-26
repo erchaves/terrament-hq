e478a33882d51d77d9cb8d606f7a34837eeade89
Eric Chaves <erchaves@gmail.com>
2019-09-26 17:09:27 -0400

    init

---
+++ src/Components/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './index.scss';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        hello world
+      </div>
+    );
+  }
+}
+
+export default Home;
