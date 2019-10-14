436483c830e7ce94f96fa1f6ffd0036217ef0518
Eric Chaves <erchaves@gmail.com>
2019-10-13 22:17:58 -0500

    text updates

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -63,32 +63,61 @@ class Home extends React.Component {
 
             <div className="content-section">
               <p>
-                Terrament is an energy startup ready to solve our trillion-dollar energy storage crisis. <br/>
-              </p>
-              <p>
-                We are building large scale grid energy storage using Underground Pumped Storage, or UPHS. We estimate UPHS to be 3-15 times cheaper than lithium ion over a 40 year <a href="https://en.wikipedia.org/wiki/Cost_of_electricity_by_source" target="_blank" rel="noopener noreferrer">LCOE</a>.
+                Terrament is an energy startup solving our trillion-dollar energy storage crisis. <br/>
               </p>
             </div>
 
+            <div className="content-section">
+              <h3 className="h3">
+                Solution
+              </h3>
+              <p>
+                Terrament's design uses a technology called Underground Pumped Hydro Storage, or UPHS. UPHS is a simple, well-researched technology that can provide scalable energy storage at an afforable cost. We estimate our design to be 3-15 times cheaper than lithium ion storage over a lifetime of forty years.
+              </p>
+            </div>
+
+            <div className="content-section">
+              <h3 className="h3">
+                Summary
+              </h3>
+              <p>
+                <a href="https://www.scribd.com/embeds/427763238/content?start_page=1&view_mode=slideshow" target="_blank" rel="noopener noreferrer"
+                >
+                  Wiew our summary deck to learn more
+                </a>.
+              </p>
+            </div>
+
+            <div className="content-section">
+              <h3 className="h3">
+                Why Terrament?
+              </h3>
+              <p>
+                World governments have promised to quit fossil fuels by 2050. But we are not yet on track to meet those promises. <a href="https://www.eto.think.woodmac.com" target="_blank" rel="noopener noreferrer">Research shows</a> that if we don't expand our energy storage capacity, our solar and wind energy markets will stagnate and fail to reach even 25% of the energy market by 2040. This will cause global temperatures to rise over 3°C, a level which will cause catastrophic climate damage. We must do better.
+              </p>
+              <p>
+                We need highly-scalable energy storage, and we need it immediately. Terrament provides a low-risk solution for an afforable cost.
+              </p>
+            </div>
             <div className="content-section">
               <h3 className="h3">
                 Links
               </h3>
               <ul className="info-link-list">
                 <li className="info-link">
-                  <a href="https://github.com/syllable-hq/uphs-feasibility-study" target="_blank" rel="noopener noreferrer">
-                    Feasibility study
-                  </a>: &nbsp;
-                  <span>
-                    Our feasibility report about Underground Pumped Storage.
-                  </span>
-                </li>
-                <li className="info-link">
                   <a href="https://www.scribd.com/embeds/427763238/content?start_page=1&view_mode=slideshow" target="_blank" rel="noopener noreferrer">
                     Pitch deck
                   </a>: &nbsp;
                   <span>
                     Our pitch deck (draft). This is not yet intended for investors. Its purpose is to find a civil engineer co-founder. Interested? <a href="mailto:hello@terramenthq.com" target="_blank" rel="noopener noreferrer">Contact us</a>.
+                  </span>
+                </li>
+                <li className="info-link">
+                  <a href="https://github.com/syllable-hq/uphs-feasibility-study" target="_blank" rel="noopener noreferrer">
+                    Feasibility study
+                  </a>: &nbsp;
+                  <span>
+                    Our feasibility report about Underground Pumped Storage.
                   </span>
                 </li>
               </ul>
@@ -145,12 +174,13 @@ class Home extends React.Component {
             <h2 className="h2">
               Contact Us
             </h2>
-
+            <br />
+            <p>
+              Terrament is currently seeking a civil engineer co-founder to join our team. <br/>Any introductions are greatly appreciated.
+            </p>
+            <br />
             <p>
               Got questions or feedback? Please <a href="mailto:hello@terramenthq.com" target="_blank" rel="noopener noreferrer">email us</a>.
-            </p>
-            <p>
-              Terrament is currently seeking a civil engineer co-founder to join our team. <br/>Any introductions are greatly appreciated. See our <a href="https://www.scribd.com/embeds/427763238/content?start_page=1&view_mode=slideshow" target="_blank" rel="noopener noreferrer">pitch deck</a> for more information.
             </p>
             <div className="footer">
               <div className="footer-inner">
