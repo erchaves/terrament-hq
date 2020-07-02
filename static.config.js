08acea9a2f6185c966eb36195c16cc1b83cd142b
Eric Chaves <erchaves@gmail.com>
2020-07-02 18:26:00 -0500

    metatag updages

--- static.config.js
+++ static.config.js
@@ -6,10 +6,11 @@ import React from 'react';
 // static.config.js
 export default {
   getSiteData: () => ({
-    title: 'Terrament',
+    title: 'Terrament | Radically Scalable Energy Storage',
     siteRoot: siteRoot,
-    description: 'Terrament is an energy startup solving our trillion-dollar energy storage crisis. We are using Underground Pumped Hydro Energy Storage which is cheaper and less risky at scale than lithium ion technology.',
+    description: 'Terrament is solving our trillion-dollar energy storage crisis. We are using Underground Gravity Storage to build low risk high scale energy storage that is far cheaper than Li-ion battery technology.',
     imageUrl: `${siteRoot}/terrament-logo.png`,
+    imageWideUrl: `${siteRoot}/terrament-logo-text.png`,
     type: 'website',
   }),
   plugins: ["react-static-plugin-sass"],
@@ -38,12 +39,13 @@ export default {
     Head,
     Body,
     children,
-    state: { siteData, renderMeta },
+    state: { siteData },
   }) => (
     <Html lang="en-US">
       <Head>
         <meta charSet="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
         <link rel="preload" as="font" href="/fonts/Matter-Regular.woff2" type="font/woff2" crossOrigin="anonymous"/>
         <link rel="preload" as="font" href="/fonts/Matter-Regular.woff" type="font/woff" crossOrigin="anonymous"/>
         <link rel="preload" as="font" href="/fonts/Matter-SemiBold.woff2" type="font/woff2" crossOrigin="anonymous"/>
@@ -55,13 +57,13 @@ export default {
 
         <meta property="og:title" content={siteData.title} />
         <meta property="og:description" content={siteData.description} />
-        <meta property="og:image" content={siteData.imageUrl} />
+        <meta property="og:image" content={siteData.imageWideUrl} />
         <meta property="og:type" content={siteData.type} />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:title" content={siteData.title} />
         <meta property="twitter:description" content={siteData.description} />
-        <meta property="twitter:image" content={siteData.imageUrl} />
+        <meta property="twitter:image" content={siteData.imageWideUrl} />
       </Head>
       <Body>{children}</Body>
     </Html>
